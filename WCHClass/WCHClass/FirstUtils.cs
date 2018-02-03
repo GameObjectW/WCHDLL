0c576faa95f81da18c740ae93bd707129ab14ecb
gameobjectw <gameobjectw@163.com>
2018-02-03 15:49:30 +0800

    增加范围取数

--- WCHClass/WCHClass/FirstUtils.cs
+++ WCHClass/WCHClass/FirstUtils.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Math
 {
@@ -29,6 +30,47 @@ namespace Math
         public static Vector3 RotateVector3WithAixsRandom(Vector3 startVector, Vector3 aix)
         {
             return Quaternion.AngleAxis(Random.Range(0f, 360f), aix) * startVector.normalized;
+        }
+        public static float[] GetIntsFromRangeNoRepeat(float Mix, float Max, int Num)
+        {
+            float[] value = new float[Num];
+            List<float> list = new List<float>();
+            for (int i = 0; i < value.Length; i++)
+            {
+                while (true)
+                {
+                    float Rvalue = Random.Range(Mix, Max);
+                    if (!list.Contains(t => Math.Abs(t - Rvalue) > 0.01f)) continue;
+                    list.Add(Rvalue);
+                    value[i] = Rvalue;
+                    break;
+                }
+            }
+            return value;
+        }
+        /// <summary>
+        /// 在一定范围内随机获取N个数（float）
+        /// </summary>
+        /// <param name="Mix">随机最小边界（包含）</param>
+        /// <param name="Max">随机最大边界（未包含）</param>
+        /// <param name="Num">获取数量</param>
+        /// <returns></returns>
+        public static int[] GetIntsFromRangeNoRepeat(int Mix, int Max, int Num)
+        {
+            int[] value = new int[Num];
+            List<int> list = new List<int>();
+            for (int i = 0; i < value.Length; i++)
+            {
+                while (true)
+                {
+                    int Rvalue = Random.Range(Mix, Max);
+                    if (!list.Contains(t => t - Rvalue == 0)) continue;
+                    list.Add(Rvalue);
+                    value[i] = Rvalue;
+                    break;
+                }
+            }
+            return value;
         }
     }
 }
